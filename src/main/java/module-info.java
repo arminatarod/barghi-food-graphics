6550d559540ad88eafc6ab054945a36beca7cb7c
Armin Atarod <armin.atarod@gmail.com>
2023-06-27 14:00:25 +0330

    Commit message

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.barghifoodgraphics {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.barghifoodgraphics to javafx.fxml;
