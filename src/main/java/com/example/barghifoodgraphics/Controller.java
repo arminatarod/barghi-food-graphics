39a27f5f00400cd28530b54d51e46ceb322d3ec3
Armin Atarod <armin.atarod@gmail.com>
2023-06-27 13:49:23 +0330

    Commit message

--- src/main/java/com/example/barghifoodgraphics/Controller.java
+++ src/main/java/com/example/barghifoodgraphics/Controller.java
@@ -3,7 +3,7 @@ package com.example.barghifoodgraphics;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
