07a99cabee6763ac16862c671e476a7d30675221
Armin Atarod <armin.atarod@gmail.com>
2023-06-27 16:26:53 +0330

    Commit message

--- src/main/java/com/example/barghifoodgraphics/MainApplication.java
+++ src/main/java/com/example/barghifoodgraphics/MainApplication.java
@@ -9,13 +9,15 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
     static Stage stage;
-    static Scene login, signup;
+    static Scene login, signup, userPage;
     @Override
     public void start(Stage mainStage) throws IOException {
         FXMLLoader fxmlLoaderLogin = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
         login = new Scene(fxmlLoaderLogin.load(), 400, 600);
         FXMLLoader fxmlLoaderSignup = new FXMLLoader(MainApplication.class.getResource("signup.fxml"));
         signup = new Scene(fxmlLoaderSignup.load(), 400, 600);
+        FXMLLoader fxmlLoaderUserPage = new FXMLLoader(MainApplication.class.getResource("userPage.fxml"));
+        userPage = new Scene(fxmlLoaderUserPage.load(), 400, 600);
         stage = mainStage;
         stage.setTitle("Barghi Food");
         stage.setMinWidth(400);
---
+++ src/main/java/com/example/barghifoodgraphics/userPageController.java
@@ -0,0 +1,9 @@
+package com.example.barghifoodgraphics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class userPageController {
+
+}
