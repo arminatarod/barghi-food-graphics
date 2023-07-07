a78293fb2624408466884112a1fca944ee2f6e28
Armin Atarod <armin.atarod@gmail.com>
2023-07-07 10:30:19 +0330

    Commit message

--- src/main/java/com/example/barghifoodgraphics/loginController.java
+++ src/main/java/com/example/barghifoodgraphics/loginController.java
@@ -1,15 +1,41 @@
 package com.example.barghifoodgraphics;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class loginController {
     @FXML
     private TextField usernameField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private CheckBox captchaBox;
+    @FXML
+    private Pane captchaPane;
+    public void forgotPressed() {
+
+    }
+    public void captchaPressed() {
+        if (captchaBox.isSelected()) {
+            captchaPane.setOpacity(0);
+            FadeTransition ft = new FadeTransition(Duration.millis(500), captchaPane);
+            ft.setFromValue(1);
+            ft.setToValue(0);
+            ft.play();
+        }
+    }
     public void loginPressed() {
-        Main.core.login(usernameField.getText(), passwordField.getText());
+        if (captchaBox.isSelected()) {
+            //Main.core.login(usernameField.getText(), passwordField.getText());
+        } else {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setHeaderText("Captcha error!");
+            a.setContentText("You must confirm that you are not a robot.");
+            a.show();
+        }
     }
     public void changeToSignup() {
         MainApplication.stage.setScene(MainApplication.signup);
--- src/main/java/com/example/barghifoodgraphics/signupController.java
+++ src/main/java/com/example/barghifoodgraphics/signupController.java
@@ -1,9 +1,10 @@
 package com.example.barghifoodgraphics;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 public class signupController {
     @FXML
@@ -12,13 +13,33 @@ public class signupController {
     private PasswordField passwordField;
     @FXML
     private ChoiceBox typeChooser;
+    @FXML
+    private CheckBox captchaBox;
+    @FXML
+    private Pane captchaPane;
+    public void captchaPressed() {
+        if (captchaBox.isSelected()) {
+            captchaPane.setOpacity(0);
+            FadeTransition ft = new FadeTransition(Duration.millis(500), captchaPane);
+            ft.setFromValue(1);
+            ft.setToValue(0);
+            ft.play();
+        }
+    }
     public void signupPressed() {
-        if (typeChooser.getValue().equals("User"))
-            Main.core.addUser(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());
-        else if (typeChooser.getValue().equals("Administrator"))
-            Main.core.addAdmin(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());
-        else
-            Main.core.addDelivery(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());
+        if (captchaBox.isSelected()) {
+            /*if (typeChooser.getValue().equals("User"))
+                Main.core.addUser(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());
+            else if (typeChooser.getValue().equals("Administrator"))
+                Main.core.addAdmin(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());
+            else
+                Main.core.addDelivery(usernameField.getText(), passwordField.getText(), recoveryQuestion.getText(), recoveryAnswer.getText());*/
+        } else {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setHeaderText("Captcha error!");
+            a.setContentText("You must confirm that you are not a robot.");
+            a.show();
+        }
     }
     public void changeToLogin() {
         MainApplication.stage.setScene(MainApplication.login);
