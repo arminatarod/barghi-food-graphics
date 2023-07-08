b4f747dd4be312eadf1c3c0f8b74d02d6f091dbb
Armin Atarod <armin.atarod@gmail.com>
2023-07-09 01:43:26 +0330

    supermarket update

---
+++ src/main/java/com/example/barghifoodgraphics/menuController.java
@@ -0,0 +1,31 @@
+package com.example.barghifoodgraphics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class menuController {
+    @FXML TabPane myTabPane;
+    @FXML Button CommentsButton;
+    @FXML Label  averageRatingLabel, BackLabel;
+    public void initialize()
+    {
+        myTabPane = new TabPane();
+        List<String> foodTypes = Arrays.asList("Appetizers", "Entrees", "Desserts", "Pizza", "Pasta", "Drinks", "Chicken", "Steak", "Coffee");
+        for(String FoodType : foodTypes)
+        {
+            Tab tab = new Tab(FoodType);
+            ListView<String> listView = new ListView<>();
+            for(int i=1;i<16;i++)
+            {
+                listView.getItems().add("Food" + i);
+            }
+            listView.setPrefSize(400,402);
+            tab.setContent(listView);
+            myTabPane.getTabs().add(tab);
+        }
+        averageRatingLabel.setText(Double.toString(4.8));
+    }
+}
